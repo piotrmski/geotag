2de9b7e4fc57b1f59c7639a952e282eb07e6ecb1
Piotr Marczyński <piotrmski@gmail.com>
2020-04-02 01:19:57 +0200

    Removed accidental leftover, commited package-lock.json

--- src/main.js
+++ src/main.js
@@ -2,15 +2,6 @@ import Vue from 'vue'
 import App from './App.vue'
 import "leaflet/dist/leaflet.css"
 
-import { Icon } from 'leaflet';
-
-delete Icon.Default.prototype._getIconUrl;
-Icon.Default.mergeOptions({
-  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
-  iconUrl: require('leaflet/dist/images/marker-icon.png'),
-  shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
-});
-
 new Vue({
   render: h => h(App),
   dev: {
